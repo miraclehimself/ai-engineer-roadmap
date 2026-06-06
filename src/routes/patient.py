3933b9a06a2ccfa961eca0cc20ca8c56ed3505c4
Abiola Miracle Aderiye <aderiyebiola@gmail.com>
2026-06-06 20:51:34 +0100

    Add HTTP error handling to patient API

--- src/routes/patient.py
+++ src/routes/patient.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src.services.patient_service import validate_patient
 
 router = APIRouter()
@@ -8,4 +8,17 @@ def get_patient(patient_id: str):
 
     result = validate_patient(patient_id)
 
+    if result["valid"] is False:
+
+        if result ["message"] == "Patient not found":
+            raise HTTPException(
+                status_code=404, 
+                detail=result["message"]
+            )
+    
+        raise HTTPException(
+            status_code=400,
+            detail=result["message"]
+        )
+    
     return result
