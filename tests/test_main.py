b9a28c6d8affc05df491e3aaec42e0866f46f0aa
Abiola Miracle Aderiye <aderiyebiola@gmail.com>
2026-06-09 22:22:09 +0100

    Add endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,16 @@ def test_health_check():
 
     assert response.status_code == 200
     assert response.json()["status"] == "healthy"
+
+def test_get_active_patient():
+    response = client.get("/patient/77777")
+
+    assert response.status_code == 200
+    assert response.json()["valid"] is True
+    assert response.json()["patient"]["status"] == "Active"
+
+def test_patient_not_found():
+    response = client.get("/patient/99999")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Patient not found"
