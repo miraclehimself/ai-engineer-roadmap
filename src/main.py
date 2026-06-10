f3f8aab6b769b90ce0f5539544c1fed350200423
Abiola Miracle Aderiye <aderiyebiola@gmail.com>
2026-06-10 19:28:37 +0100

    Add info endpoint

--- src/main.py
+++ src/main.py
@@ -13,4 +13,13 @@ def health_check():
         "service": "ai-engineer-roadmap-api"
     }
 
+@app.get("/info")
+def get_info():
+    return {
+        "system": "AI Engineer Roadmap",
+        "version":"1.0",
+        "environment": "development"
+    }
+    
+
 app.include_router(patient_router)
